dee6443c76e9271a86c434e708dd3db31595c585
S.B <65360079+fish-hue@users.noreply.github.com>
2023-03-22 09:54:03 -0500

    Update nscrape.sh
    saves output to file and scans ports 80, 440, and 8080 by default

--- nscrape.sh
+++ nscrape.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-read -p "Enter the URL to scan: " url
+read -p "Enter the URL or IP to scan: " url
 read -p "Enter '4' to scan IPv4 or '6' to scan IPv6: " ip_version
-read -p "Enter the port number to use for scanning: " port
 read -p "Enter the maximum testing time per host in seconds: " max_time
 read -p "Enter a list of plugins to run (leave blank for all): " plugins
 read -p "Enter the starting directory for scanning: " root
@@ -13,36 +12,47 @@ read -p "Enter the pause time between tests in seconds: " pause
 read -p "Disable SSL (Y/N): " no_ssl
 read -p "Check database and key files for syntax errors (Y/N): " db_check
 
-# set flags based on user input
-if [ $ip_version -eq 4 ]; then
-  ip_flag="-ipv4"
-else
-  ip_flag="-ipv6"
+# Construct the Nikto command based on user inputs
+nikto_command="nikto -host $url -maxtime $max_time -root $root -pause $pause -Format txt"
+
+if [ "$ip_version" == "4" ]; then
+    nikto_command="$nikto_command -ipv4"
+elif [ "$ip_version" == "6" ]; then
+    nikto_command="$nikto_command -ipv6"
 fi
 
-if [ -n "$plugins" ]; then
-  plugins_flag="-Plugins $plugins"
+if [ ! -z "$plugins" ]; then
+    nikto_command="$nikto_command -Plugins $plugins"
 fi
 
-if [ "$mutate" = "Y" ]; then
-  mutate_flag="-mutate"
+if [ "$mutate" == "Y" ]; then
+    nikto_command="$nikto_command -mutate"
 fi
 
-if [ "$user_agent" = "Y" ]; then
-  user_agent_flag="-useragent Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+if [ "$user_agent" == "Y" ]; then
+    read -p "Enter the custom user agent: " user_agent_value
+    nikto_command="$nikto_command -useragent $user_agent_value"
 fi
 
-if [ "$use_proxy" = "Y" ]; then
-  use_proxy_flag="-useproxy"
+if [ "$use_proxy" == "Y" ]; then
+    read -p "Enter the proxy URL: " proxy_url
+    nikto_command="$nikto_command -useproxy $proxy_url"
 fi
 
-if [ "$no_ssl" = "Y" ]; then
-  no_ssl_flag="-nossl"
+if [ "$no_ssl" == "Y" ]; then
+    nikto_command="$nikto_command -nossl"
 fi
 
-if [ "$db_check" = "Y" ]; then
-  db_check_flag="-dbcheck"
+if [ "$db_check" == "Y" ]; then
+    nikto_command="$nikto_command -dbcheck"
 fi
 
-# run nikto with the specified flags
-nikto $ip_flag -port $port -maxtime $max_time $plugins_flag -root $root $mutate_flag $user_agent_flag $use_proxy_flag -Pause $pause $no_ssl_flag $db_check_flag -h $url
+# Run the Nikto command on ports 80, 440, and 8080 can be adjusted as needed
+nikto_command="$nikto_command -port 80,440,8080"
+echo "Running the following Nikto command: $nikto_command"
+$nikto_command
+
+# Save the output to a file named after the IP or URL entered
+output_file="$url.txt"
+echo "Saving the output to file: $output_file"
+$nikto_command -output $output_file
